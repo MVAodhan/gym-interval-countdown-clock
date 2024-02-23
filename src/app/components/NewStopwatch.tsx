93e54cf12854290b47018d4f6cf0af0e0734c7cf
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-02-24 04:25:56 +1300

    progress kinda I hope

--- src/app/components/NewStopwatch.tsx
+++ src/app/components/NewStopwatch.tsx
@@ -20,10 +20,19 @@ import {
 
 import { Button } from "@/components/ui/button";
 
+interface GymSet {
+  timeMS: number;
+  rest: boolean;
+}
+
+interface Phase {
+  numSets: number;
+  sets: GymSet[];
+}
 const Stopwatch = () => {
   const inputData = [
-    { timeMS: 20000, sets: 4, rests: false },
-    { timeMS: 240000, sets: 4, rests: true, restMs: 20000 },
+    // { timeMS: 10000, sets: 2, rests: false, restMS: 0 },
+    { timeMS: 40000, sets: 4, rests: true, restMS: 5000 },
   ];
   const numberSound = new Howl({
     src: ["countdown_number.mp3"],
@@ -34,23 +43,15 @@ const Stopwatch = () => {
   const [isRunning, setIsRunning] = useState(false);
 
   const [time, setTime] = useState(0);
+  const [phases, setPhases] = useState<Phase[]>([]);
 
   const [phaseNum, setPhaseNum] = useState(0);
   const [setNum, setSetNum] = useState(0);
 
-  const [phases, setPhases] = useState<Phase[]>([]);
+  const [setDisplay, setSetDisplay] = useState(1);
+  const [phaseDisplay, setPhaseDisplay] = useState(1);
 
   const timerRef = useRef<any>();
-
-  interface GymSet {
-    timeMS: number;
-    rest: boolean;
-  }
-
-  interface Phase {
-    numSets: number;
-    sets: GymSet[];
-  }
 
   const getMinutes = (ms: number) =>
     ("0" + Math.floor((ms / 60 / 1000) % 60)).slice(-2);
@@ -72,7 +73,7 @@ const Stopwatch = () => {
     let phaseRests: GymSet[] = [];
     let restLen = 0;
 
-    if (rests !== false) {
+    if (rests !== false && restMS !== 0) {
       for (let i = 0; i < sets - 1; i++) {
         phaseRests = [...phaseRests, { timeMS: restMS, rest: true }];
         restLen = restLen + 1;
@@ -89,7 +90,7 @@ const Stopwatch = () => {
 
     let { phaseRests, restLen } = calculateRests(
       inputData[i].sets,
-      inputData[i].restMs as number,
+      inputData[i].restMS,
       inputData[i].rests
     );
 
@@ -149,6 +150,7 @@ const Stopwatch = () => {
       currentPhase = phases[phaseNum];
     }
 
+    //Todo: impliment changing time logic
     if (isRunning) {
       if (time === 3000 || time === 2000 || time === 1000) {
         numberSound.play();
@@ -158,19 +160,25 @@ const Stopwatch = () => {
       }
 
       if (time === 0) {
-        if (currentPhase) {
+        if (phaseNum === phases.length) {
+          setIsRunning(false);
         }
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [time]);
+  }, [time, phaseNum]);
 
   return (
     <div className=" w-4/5 flex flex-col">
       <div
         className={`flex flex-col items-center text-[200px] text-[#ff1717] ${inter.className}`}
       >
-        <div className="text-2xl">SET : {setNum}</div>
+        <div className="text-2xl">
+          {" "}
+          {isRunning && `PHASE : ${phaseDisplay}`}
+        </div>
+        <div className="text-2xl">{isRunning && `SET : ${setDisplay}`}</div>
+
         {formatTime(time)}
       </div>
       <div className="w-full flex justify-between">
@@ -178,7 +186,7 @@ const Stopwatch = () => {
           onClick={() => {
             setIsRunning(true);
             setPhaseNum(0);
-            setTime(phases[phaseNum].sets[0].timeMS);
+            setTime(phases[phaseNum].sets[setNum].timeMS);
           }}
         >
           Start
@@ -192,7 +200,9 @@ const Stopwatch = () => {
           <button
             onClick={() => {
               setPhaseNum(0);
+              setPhaseDisplay(0);
               setSetNum(0);
+              setSetDisplay(1);
               setTime(0);
             }}
           >
